fdc4a2995c35d236ed0acc3af42dab0ff36c73ba
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2024-01-18 13:44:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function convertToRoman(num) {
   	const obj = {
-      0:['M',1000], 
+      0:['M',1000],
       1:['D', 500], 
       2:['C', 100], 
       3:['L', 50], 
@@ -8,7 +8,17 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
-
+	let numbers = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
+	let symbols = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
+	let ans = "";
+	while(num > 0) {
+		let i = 0;
+		while(num <= numbers[i] && i < numbers.length) {
+			i++;
+		}
+		ans += symbols[i - 1];
+	}
+	return ans;
   //your code here
 
 }
